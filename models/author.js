a959965c78ed48f3325ebe6abc6bfc916c988705
jared <38332939+jaredaritter@users.noreply.github.com>
2020-12-24 10:02:17 -0500

    Models created

---
+++ models/author.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxlength: 100 },
+  family_name: { type: String, required: true, maxlength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+// Virtuals
+AuthorSchema.virtual('name').get(function () {
+  return `${this.family_name}, ${this.first_name}`;
+});
+
+AuthorSchema.virtual('lifespan').get(function () {
+  return (
+    this.data_of_death.getYear() - this.date_of_birth.getYear()
+  ).toString();
+});
+
+AuthorSchema.virtual('url').get(function () {
+  return `/catalog/author/${this._id}`;
+});
+
+// Export module
+module.exports = mongoose.model('Author', AuthorSchema);
