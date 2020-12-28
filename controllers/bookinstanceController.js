30b94b38f47c6e56a3ced3a30dc582f15ec9b629
jared <38332939+jaredaritter@users.noreply.github.com>
2020-12-28 16:55:26 -0500

    Add date library and update due date formatting.

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -5,13 +5,13 @@ const BookInstance = require('../models/bookinstance');
 exports.bookinstance_list = function (req, res) {
   BookInstance.find()
     .populate('book')
-    .exec(function (err, data) {
+    .exec(function (err, list_bookinstances) {
       if (err) {
         return next(err);
       }
       res.render('bookinstance_list', {
         title: 'Book Instance List',
-        bookinstance_list: data,
+        bookinstance_list: list_bookinstances,
       });
     });
 };
