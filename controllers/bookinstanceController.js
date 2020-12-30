e6c18faa6046f5607978f44b12c16c2a35e935c2
jared <38332939+jaredaritter@users.noreply.github.com>
2020-12-30 16:28:17 -0500

    Add bookinstance detail controller and view.

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,4 +1,3 @@
-const { nextTick } = require('async');
 const BookInstance = require('../models/bookinstance');
 
 // Display list of all BookInstances.
@@ -17,8 +16,23 @@ exports.bookinstance_list = function (req, res) {
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+exports.bookinstance_detail = function (req, res, next) {
+  BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function (err, bookinstance) {
+      if (err) {
+        return next(err);
+      }
+      if (bookinstance === null) {
+        const err = new Error('Book Instance not found.');
+        err.status = 404;
+        return next(err);
+      }
+      res.render('bookinstance_detail', {
+        title: `Copy: ${bookinstance.book.title}`,
+        bookinstance: bookinstance,
+      });
+    });
 };
 
 // Display BookInstance create form on GET.
