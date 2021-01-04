ca90ba53ffec445e5624566c0d848f019757d3a8
jared <38332939+jaredaritter@users.noreply.github.com>
2021-01-04 11:49:05 -0500

    Add error class.

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -7,7 +7,7 @@ const { body, validationResult } = require('express-validator');
 // Display list of all Authors.
 exports.author_list = function (req, res, next) {
   Author.find()
-    .sort([['family_name', 'ascending']])
+    .sort({ family_name: 1 })
     .exec(function (err, list_authors) {
       if (err) {
         return next(err);
@@ -28,7 +28,7 @@ exports.author_detail = function (req, res, next) {
       },
       author_books: function (callback) {
         Book.find({ author: req.params.id })
-          .sort([['title', 'ascending']]) // MIGHT NEED TO POPULATE FOR GENRE
+          .sort({ title: 1 }) // MIGHT NEED TO POPULATE FOR GENRE
           .exec(callback);
       },
     },
@@ -52,13 +52,77 @@ exports.author_detail = function (req, res, next) {
 
 // Display Author create form on GET.
 exports.author_create_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: Author create GET');
+  Author.find({}, 'first_name family_name')
+    .sort({ family_name: 1 })
+    .exec(function (err, list_authors) {
+      if (err) {
+        return next(err);
+      }
+      res.render('author_form', {
+        title: 'Create Author',
+        author_list: list_authors,
+      });
+    });
 };
 
 // Handle Author create on POST.
-exports.author_create_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+  body('first_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('First name must be specified.')
+    .isAlphanumeric()
+    .withMessage('First name has non-alphanumeric characters.'),
+  body('family_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Family name must be specified.')
+    .isAlphanumeric()
+    .withMessage('Family name has non-alphanumeric characters.'),
+  body('date_of_birth', 'Invalid date of birth.')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body('date_of_death', 'Invalid date of death.')
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      Author.find({}, 'first_name family_name')
+        .sort({ family_name: 1 })
+        .exec(function (err, list_authors) {
+          if (err) {
+            return next(err);
+          }
+          res.render('author_form', {
+            title: 'Create Author',
+            author: req.body,
+            errors: errors.array(),
+            author_list: list_authors,
+          });
+        });
+      return; // NEEDED TO CLOSE FUNCTION?
+    } else {
+      const author = new Author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+      });
+      author.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(author.url);
+      });
+    }
+  },
+];
 
 // Display Author delete form on GET.
 exports.author_delete_get = function (req, res, next) {
