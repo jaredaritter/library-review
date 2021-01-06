f4c44be1e88aec92d03f30ab5e90d785e7c63f00
jared <38332939+jaredaritter@users.noreply.github.com>
2021-01-06 11:24:55 -0500

    Add book controller form and controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -85,13 +85,111 @@ exports.book_detail = function (req, res, next) {
 
 // Display book create form on GET.
 exports.book_create_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: Book create GET');
+  async.parallel(
+    {
+      authors: function (callback) {
+        Author.find().sort({ family_name: 1 }).exec(callback);
+      },
+      genres: function (callback) {
+        Genre.find().sort({ name: 1 }).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      res.render('book_form', {
+        title: 'Create Book',
+        authors: results.authors,
+        genres: results.genres,
+      });
+    }
+  );
 };
 
 // Handle book create on POST.
-exports.book_create_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: Book create POST');
-};
+exports.book_create_post = [
+  // CONVERT GENRE TO ARRAY
+  // REQUIRED BECAUSE A BOOK CAN HAVE MULTIPLE GENRES. WANT CONSISTENCY IN DATA TYPE WHEN STORED. GENRE IN THE BOOK MODEL IS AN ARRAY.
+  (req, res, next) => {
+    if (!(req.body.genre instanceof Array)) {
+      // IF UNDEFINED STORE EMPTY ARRAY
+      if (typeof req.body.genre === 'undefined') req.body.genre = [];
+      // OTHERWISE CONVERT TO ARRAY CONTAINING VALUES OF REQ.BODY.GENRE
+      else {
+        req.body.genre = new Array(req.body.genre);
+      }
+    }
+    // SEND TO NEXT MIDDLEWARE
+    next();
+  },
+
+  // VALIDATE FORM DATA
+  body('title', 'Title must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('author', 'Author must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('summary', 'Summary must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('genre.*').escape(),
+
+  // PROCESS REQUEST
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      summary: req.body.summary,
+      isbn: req.body.isbn,
+      genre: req.body.genre,
+    });
+
+    if (!errors.isEmpty()) {
+      async.parallel(
+        {
+          authors: function (callback) {
+            Author.find().sort({ family_name: 1 }).exec(callback);
+          },
+          genres: function (callback) {
+            Genre.find().sort({ name: 1 }).exec(callback);
+          },
+        },
+        function (err, results) {
+          if (err) {
+            return next(err);
+          }
+          for (let i = 0; i < results.genres.length; i++) {
+            if (book.genre.indexOf(results.genres[i]._id) > -1) {
+              results.genres[i].checked = 'true';
+            }
+          }
+          res.render('book_form', {
+            title: 'Create Book',
+            authors: results.authors,
+            genres: results.genres,
+            book: book,
+            errors: errors.array(),
+          });
+          return;
+        }
+      );
+    } else {
+      book.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(book.url);
+      });
+    }
+  },
+];
 
 // Display book delete form on GET.
 exports.book_delete_get = function (req, res, next) {
